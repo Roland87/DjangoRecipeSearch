218b0305bea7cf136a25118dfed37fc496f12e7d
rolandrebek <roland.rebek87@gmail.com>
2024-04-19 10:35:21 +0100

    Start of the RealRecipeFinder project django is connected to a postgresql database witch is runnug in a separate docker container

---
+++ RecipeFinderProject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for RecipeFinderProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RecipeFinderProject.settings')
+
+application = get_wsgi_application()
