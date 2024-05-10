4cc3cb16d49ee466e4748152b197528050313449
rolandrebek <roland.rebek87@gmail.com>
2024-05-10 13:33:30 +0100

    Mis spelling corected

--- recipe_search/admin.py
+++ recipe_search/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import UserProfile, UserRating, MealCategory, Ingredient, Reipe, RecipeIngredient, RecipeImage
+from .models import UserProfile, UserRating, MealCategory, Ingredient, Recipe, RecipeIngredient, RecipeImage
 
 admin.site.register(UserProfile)
 admin.site.register(UserRating)
 admin.site.register(MealCategory)
 admin.site.register(Ingredient)
-admin.site.register(Reipe)
+admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
 admin.site.register(RecipeImage)
