cd7a93550cdd0bfeb6ee4e577a79dc5f21604daa
rolandrebek <roland.rebek87@gmail.com>
2024-05-10 13:26:50 +0100

    Registered the model input in the admin site.

--- recipe_search/admin.py
+++ recipe_search/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile, UserRating, MealCategory, Ingrediente, Reipe, RecipeIngredient, RecipeImage
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserRating)
+admin.site.register(MealCategory)
+admin.site.register(Ingrediente)
+admin.site.register(Reipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(RecipeImage)
